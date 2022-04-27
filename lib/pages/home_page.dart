5eb44c3f4ca9a5ec56760ce4fa3df05c536fa1e8
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-04-27 23:23:47 +0700

    Add Alertdialog and setstate

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late double _deviceWidth, _deviceHeight;
+  String? _newTaskContent;
 
   _HomePageState();
 
@@ -18,6 +19,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
+    print("Input Value: $_newTaskContent");
 
     return Scaffold(
       appBar: AppBar(
@@ -57,11 +59,30 @@ class _HomePageState extends State<HomePage> {
 
   Widget _addTaskButton() {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: _displayTaskPopup,
       child: const Icon(
         Icons.add,
         color: Colors.white,
       ),
+    );
+  }
+
+  void _displayTaskPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext _context) {
+        return AlertDialog(
+          title: const Text("Add new task"),
+          content: TextField(
+            onSubmitted: (_value) {},
+            onChanged: (_value) {
+              setState(() {
+                _newTaskContent = _value;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
